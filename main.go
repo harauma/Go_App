c8ccceb3d6b6523fb9aebaf613b72da0e77fc6bd
harauma <fot39hara@gmail.com>
2021-05-14 01:53:34 +0900

    Todo取得処理作成

--- main.go
+++ main.go
@@ -37,6 +37,22 @@ func main() {
 	// u, _ = models.GetUser(1)
 	// fmt.Println(u)
 
-	user, _ := models.GetUser(2)
-	user.CreateTodo("First Todo")
+	// user, _ := models.GetUser(2)
+	// user.CreateTodo("First Todo")
+
+	// t, _ := models.GetTodo(1)
+	// fmt.Println(t)
+
+	user, _ := models.GetUser(3)
+	user.CreateTodo("Third Todo")
+
+	// todos, _ := models.GetTodos()
+	// for _, v := range todos {
+	// 	fmt.Println(v)
+	// }
+	user2, _ := models.GetUser(2)
+	todos, _ := user2.GetTodosByUser()
+	for _, v := range todos {
+		fmt.Println(v)
+	}
 }
