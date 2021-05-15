52c03905b6470d543f84cb50faab6dff7aad6582
harauma <fot39hara@gmail.com>
2021-05-15 15:54:23 +0900

    todo delete機能実装

--- app/models/todos.go
+++ app/models/todos.go
@@ -95,3 +95,11 @@ func (t *Todo) UpdateTodo() error {
 	}
 	return err
 }
+func (t *Todo) DeleteTodo() error {
+	cmd := `delete from todos where id = ?`
+	_, err = Db.Exec(cmd, t.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
