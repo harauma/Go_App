fe929741047256c101c8f093ca066d1f414d47ca
harauma <fot39hara@gmail.com>
2021-05-16 19:02:31 +0900

    todo新規作成機能実装

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -20,7 +20,6 @@ func index(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/", 302)
 	} else {
 		user, err := sess.GetUserBySession()
-		log.Println(user)
 		if err != nil {
 			log.Println(err)
 		}
@@ -30,5 +29,35 @@ func index(w http.ResponseWriter, r *http.Request) {
 		}
 		user.Todos = todos
 		generateHTML(w, user, "layout", "private_navbar", "index")
+	}
+}
+
+func todoNew(w http.ResponseWriter, r *http.Request) {
+	_, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		generateHTML(w, nil, "layout", "private_navbar", "todo_new")
+	}
+}
+
+func todoSave(w http.ResponseWriter, r *http.Request) {
+	sess, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		err = r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+		user, err := sess.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		content := r.PostFormValue("content")
+		if err = user.CreateTodo(content); err != nil {
+			log.Println(err)
+		}
+		http.Redirect(w, r, "/todos", 302)
 	}
 }
